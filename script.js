6be1a5e48ffe811dddd078ecf8761366820de59a
Saurabh Gaikwad <96194597+Saurabhkg03@users.noreply.github.com>
2023-01-20 21:53:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let screen=document.getElementById("display")
-let buttons=document.querySelectorAll("btn")
-
-buttons.forEach((button)=>{
-    button.addEventListener("click"),(e)=>{
-        let value =e.target.dataset.num;
+let screen = document.getElementById("display")
+let button = document.querySelectorAll(".btn")
+button.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        let value=e.target.dataset.num;
         screen.value += value;
-    }
+    })
 })
